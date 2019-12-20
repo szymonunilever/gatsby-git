e9e08e4f5af3b1834b3c53464fb92f7fc4d7d108
Szymon Sznajder <szymon.sznajder@unilever.com>
2019-12-20 00:21:46 +0000

    kurwa mac ja

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
+    <h1>Witojcie ludziska</h1>
     <p>Witojcie w naszej bajce. Gdzie kelner lata po niebie :)</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>

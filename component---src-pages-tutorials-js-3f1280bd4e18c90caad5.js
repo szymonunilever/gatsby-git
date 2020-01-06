a833916dd4e66ff3fafaedbec9c7096acddc5734
Szymon Sznajder <szymon.sznajder@unilever.com>
2020-01-06 16:31:38 +0000

    Updates

---
+++ component---src-pages-tutorials-js-3f1280bd4e18c90caad5.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{gZtR:function(e,t,a){"use strict";a.r(t);var n=a("q1tI"),l=a.n(n),o=a("Wbzz"),c=a("Bl7J"),r=a("vrFN");t.default=function(){return l.a.createElement(c.a,null,l.a.createElement(r.a,{title:"Page two"}),l.a.createElement("h1",null,"Hi from the second page"),l.a.createElement("p",null,"Welcome to page 2"),l.a.createElement(o.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-tutorials-js-3f1280bd4e18c90caad5.js.map
